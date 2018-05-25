774c513458bfc9592e94603e417507396e2306fd
Josef Sekava <joska@joskaweb.cz>
2018-05-25 21:25:11 +0200

    style: added footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react'
 class Footer extends React.Component {
   render() {
     return (
-      <footer className="footer">
-        Tohle je footer
+      <footer className="footer text-right">
+        <span>Digitální knihovnu zrakově postižených provozuje <a href="http://www.mathilda.cz" title="přejít na stránky provozovatele">Mathilda</a>.</span>
       </footer>
     );
   }
