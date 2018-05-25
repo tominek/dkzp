28beb4a72efacf962de2482338e0174d46614ba2
Juraj Hrenák <judzinator@gmail.com>
2018-05-25 15:50:56 +0200

    Category controller

---
+++ backend/src/Controller/CategoryController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CategoryController extends Controller
+{
+    /** @var CategoryRepository */
+    private $categoryRepository;
+
+    public function __construct(CategoryRepository $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    /**
+     * @Route("/categories", name="categories")
+     * @Method("POST")
+     */
+    public function listAction()
+    {
+        $data = $this->categoryRepository->findAll();
+        return $this->json([
+            'data' => $data,
+        ], 200);
+    }
+}
