43f77c973bf9fa87ae83c09798606ba1218dd9e7
Lukáš Winkler <winklerlukas.wl@outlook.com>
2018-05-25 21:25:56 +0200

    main page grid

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {Switch, Route, BrowserRouter as Router} from 'react-router-dom'
+import {Grid, Row, Col} from 'react-bootstrap'
 import { Title, Header } from '../components'
 import * as pages from './contentPages'
 import { FourZeroFour } from './'
@@ -8,18 +9,26 @@ import { FourZeroFour } from './'
 class MainPage extends React.Component {
   render() {
     return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={pages.HomePage} />
-          <Route exact path='/autori' component={pages.AuthorsPage} />
-          <Route exact path='/kategorie' component={pages.CategoriesPage} />
-          <Route exact path='/kontakt' component={pages.ContactPage} />
-          <Route exact path='/oblibene-knihy' component={pages.FavoriteBooksPage} />
-          <Route exact path='/nove-knihy' component={pages.NewBooksPage} />
-          <Route exact path='/napoveda' component={pages.HelpPage} />
-          <Route component={FourZeroFour} />
-        </Switch>
+      <div className="main-page">
+        <Grid>
+          <Row className="show-grid">
+            <Col sm={12} md={3}>
+              <Header />
+            </Col>
+            <Col sm={12} md={9} className="text-left">
+              <Switch>
+                <Route exact path='/' component={pages.HomePage} />
+                <Route exact path='/autori' component={pages.AuthorsPage} />
+                <Route exact path='/kategorie' component={pages.CategoriesPage} />
+                <Route exact path='/kontakt' component={pages.ContactPage} />
+                <Route exact path='/oblibene-knihy' component={pages.FavoriteBooksPage} />
+                <Route exact path='/nove-knihy' component={pages.NewBooksPage} />
+                <Route exact path='/napoveda' component={pages.HelpPage} />
+                <Route component={FourZeroFour} />
+              </Switch>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     )
   }
