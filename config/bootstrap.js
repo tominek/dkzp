45bd5b0cf47163ef6ff604b4e295844e36b7eebb
Tomas Knezek <tomas.knezek@gmail.com>
2018-05-25 12:34:52 +0200

    Initial commit

---
+++ config/bootstrap.js
@@ -0,0 +1,16 @@
+/**
+ * Bootstrap
+ * (sails.config.bootstrap)
+ *
+ * An asynchronous bootstrap function that runs just before your Sails app gets lifted.
+ * > Need more flexibility?  You can also do this by creating a hook.
+ *
+ * For more information on bootstrapping your app, check out:
+ * https://sailsjs.com/config/bootstrap
+ */
+
+module.exports.bootstrap = async function(done) {
+
+  return done();
+
+};
